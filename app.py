beba6cf6fdcf7593996a2c0d3d539dadf8d9242f
Thi <nomthandazompanza@gmail.com>
2020-04-07 08:45:18 +0200

    First database model

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
+db = SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default="Anonza")
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' + str(self.id)
+
+
+
 
 all_posts = [
     {
@@ -18,8 +35,9 @@ all_posts = [
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/home/<string:name>')
 
+  
+@app.route('/home/<string:name>')
 def hello_world(name):
     return 'Hello, ' + name
 #variables from urls - for dynamic urls - things have ids that keep changing
@@ -30,10 +48,22 @@ def hello_world(name):
 #POST
 def only_get():
     return 'You can only get'
-    
-@app.route('/posts')
+
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template('posts.html', posts=all_posts)
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts=all_posts)
 
 
 
